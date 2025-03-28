e1a6569cfc3ebedf830d4540401a7cee6af47923
heeso <h2s0_@naver.com>
2025-03-28 19:08:01 +0900

    meals 페이지 레이아웃 설정, 이미지 사용하기, 컴포넌트 생성하여 사용하기

---
+++ app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function ShareMealPage() {
+  return(
+    <h2>What is yours?</h2>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
+import Link from "next/link";
+
 export default function Home() {
+
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>

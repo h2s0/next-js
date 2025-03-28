4f0e79e66327f89518f30591e6d83efc4cc5cc28
heeso <h2s0_@naver.com>
2025-03-28 16:54:11 +0900

    app 에 다른 파일 구조들 생성하기

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import Header from '@/components/header';
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJs Course!</h1>
       <p>Let&apos;s get started!</p>
---
+++ components/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+
+  // 컴포넌트 폴더를 app 폴더 밖에 위치시키게 되면 app 폴더는 라우팅 관련으로만 사용할 수 있다.
+  
+  return(
+    <>
+      <img />
+      <h1>안녕 나는 헤더얌</h1>
+    </>
+  )
+}

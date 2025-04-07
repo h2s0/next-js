cedd32ccd5f1b1a875f9f64676f5a18e3f71257d
heeso <h2s0_@naver.com>
2025-04-07 13:08:07 +0900

    개별 Meals 에 대한 Not Found 오류 전송

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,11 +1,18 @@
+import Image from 'next/image';
+
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
-import Image from 'next/image';
+
+import { notFound } from 'next/navigation';
 
 export default function MealDetailsPage({params}) {
   const meal = getMeal(params.mealSlug);
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\\n/g, '<br />');
   
   return (
     <>
